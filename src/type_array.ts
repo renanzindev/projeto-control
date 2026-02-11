aca04ee2713bd36ac79bc020b953c6f51f131763
renanzindev <renancodes@gmail.com>
2026-02-11 20:39:27 -0300

    Add type array functionality with examples for numbers and strings

--- src/type_array.ts
+++ src/type_array.ts
@@ -1,2 +1,30 @@
 
 
+//  TYPE ARRAY
+
+let numeros: number[];
+
+let filmes: string[];
+
+filmes = ['Matrix', 'Senhor dos Anéis', 'O Poderoso Chefão'];
+
+console.log('Meus filmes são: ', filmes);
+
+// ADICIONAR UM FILME NO ARRAY
+filmes.push('Homem Aranha de volta para o lar');
+
+console.log('Meus filmes são: ', filmes);
+
+// ADICIONAR UM NUMERO NO ARRAY
+numeros = [1, 2, 3, 4, 5];
+
+console.log(numeros);
+
+numeros.push(6);
+
+console.log('Meus numeros são: ', numeros);
+
+// TYPE ARRAY COM UNION TYPE
+let jogadores: (string | number)[] = ['Messi', 'Ronaldo', 'Neymar', 'Mbappé', 1, 2, 3, 4, 5];
+
+jogadores.push('CR7' ,7)
