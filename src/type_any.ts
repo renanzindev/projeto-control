957802a691bd03784c8e725e56b8499e44d22c49
renanzindev <renancodes@gmail.com>
2026-02-11 20:30:37 -0300

    Remove index.js and index.css files to clean up the project structure.

---
+++ src/type_any.ts
@@ -0,0 +1,15 @@
+
+
+// TYPE ANY é um tipo de dado que pode ser qualquer tipo de dado, NÃO É LEGAL USAR ESSE TIPO!!
+
+let precoProduto;
+
+
+precoProduto = true;
+
+precoProduto = 25.90;
+
+precoProduto = "Sujeito Programador";
+
+
+console.log(precoProduto);
---
+++ src/type_boolean.ts
@@ -0,0 +1,20 @@
+ 
+
+
+//  TYPE BOOLEAN é um tipo de dado que pode ser true ou false
+
+
+let estaAutenticado: boolean = true;
+
+
+// estaAutenticado = false;
+
+
+let codeStatus: string = '200';
+
+
+// TUDO QUE FOR DIFERENTE DE 0 SERÁ TRUE
+estaAutenticado = Boolean(codeStatus);
+
+
+console.log(estaAutenticado);
